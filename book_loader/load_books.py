7748b62a9c01a6ef78170cc036f5a13a9f105f37
karopp <karoline@familie-rabe.de>
2023-12-08 14:59:27 +0100

    parse book details from isbn

--- book_loader/load_books.py
+++ book_loader/load_books.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python3
 
-import getopt
-import os
-import sys
 import json
+import os
 
+from requests_html import HTMLSession
 
 file_path = os.path.realpath(__file__)
 script_path = os.path.dirname(file_path)
-
-# Make library available in path
-#library_paths = [
-#    os.path.join(script_path, "lib")
-#]
-
-#for p in library_paths:
-#    if not (p in sys.path):
-#        sys.path.insert(0, p)
 
 #
 # Main
 #
 
+
 def main():
-    # Set default values
-    print("Hello World")
+    ratings_path = os.path.join(script_path, "content/rating.json")
+    books_path = os.path.join(script_path, "content/books.json")
+    with open(ratings_path, "r") as ratings_file, open(books_path, "w") as books_file:
+        ratings = json.load(ratings_file)
+        books = []
+        for rating in ratings:
+            book = {}
+            print(rating["isbn"])
+            isbn = rating["isbn"]
+            url = f"https://www.genialokal.de/Suche/?q={isbn}"
+            session = HTMLSession()
+            response = session.get(url)
+            xpath = '// *[ @ id = "pagebody"] / div[2] / div[2] / div / div / div[1] / a / img'
+            img = response.html.xpath(xpath)[0].attrs["src"]
+            title_entry = response.html.xpath(xpath)[0].attrs["title"].split("-")
+            title = title_entry[0].strip()
+            author = title_entry[1].strip()
+            book["image_url"] = img
+            book["title"] = title
+            book["author"] = author
+            book["link"] = response.html.xpath(
+                '//*[@id="pagebody"]/div[2]/div[2]/div/div/div[1]/a'
+            )[0].absolute_links.pop()
+            books.append(book)
+        print(books)
+        books_file.seek(0)
+        json.dump(books, books_file, indent=4, ensure_ascii=False)
+        books_file.truncate()
+
 
 if __name__ == "__main__":
     main()
