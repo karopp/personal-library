0a9500a53ceccfa134d5f45ec9d7b9a461069fc8
karopp <karoline@familie-rabe.de>
2023-12-12 21:52:22 +0100

    add skeleton for webapp

---
+++ myBookApp/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+import booksJson from '../assets/books.json';
+
+
+interface Book {
+  image_url: String;
+  title: String;
+  author: String;
+  link: String;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatCardModule, MatIconModule, MatToolbarModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  appTitle = 'PERSONAL LIBRARY';
+  bookList: Book[] = booksJson;
+}
